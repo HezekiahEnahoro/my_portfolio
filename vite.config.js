351a2d8217be8145f77de6b2e72556d2350f7978
Ghrizzy <Franciscoking301@gmail.com>
2025-07-31 14:33:33 -0700

    vite change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/developer-portfolio/", // 👈 Your GitHub repo name
+  base: "/developer-portfolio/", 
   plugins: [react(), tailwindcss()],
 });
